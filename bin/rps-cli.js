8c8c6e1b5ac795be954a16c5d4b660b5160eebca
kaiq913 <77793337+kaiq913@users.noreply.github.com>
2023-05-06 00:09:42 -0400

    bin

--- bin/rps-cli.js
+++ bin/rps-cli.js
@@ -1,3 +1,25 @@
 #!/usr/bin/env node
 
 // Your CLI code here
+import minimist from 'minimist';
+import { rps, rulesRPS, helpRPS } from '../lib/rps.js';
+const argv = minimist(process.argv.slice(2));
+
+if (argv.h || argv.help) {
+  helpRPS();
+  process.exit();
+}
+
+if (argv.r || argv.rules) {
+  rulesRPS();
+  process.exit();
+}
+
+const shot = argv._[0] || '';
+if (shot) {
+  rps(shot.toLowerCase());
+} else {
+  console.error('Error: Missing required argument SHOT.');
+  helpRPS();
+  process.exit(1);
+}
