2b68136686db6760b9e61c921dbb14dd523e8f77
kaiq913 <77793337+kaiq913@users.noreply.github.com>
2023-05-06 00:10:59 -0400

    ls

--- bin/rpsls-cli.js
+++ bin/rpsls-cli.js
@@ -1,3 +1,26 @@
 #!/usr/bin/env node
 
 // Your CLI code here
+import minimist from 'minimist';
+import { rpsls, rulesRPSLS, helpRPSLS } from '../lib/rpsls.js';
+
+const argv = minimist(process.argv.slice(2));
+
+if (argv.h || argv.help) {
+  helpRPSLS();
+  process.exit();
+}
+
+if (argv.r || argv.rules) {
+  rulesRPSLS();
+  process.exit();
+}
+
+const shot = argv._[0] || '';
+if (shot) {
+  rpsls(shot.toLowerCase());
+} else {
+  console.error('Error: Missing required argument SHOT.');
+  helpRPSLS();
+  process.exit(1);
+}
